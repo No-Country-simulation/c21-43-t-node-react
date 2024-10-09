94a46ffcf8767750dae550fbe43929fff6be4939
genac4 <genacura4@gmail.com>
2024-10-08 21:11:06 -0300

    fix done .env y dist

--- backend/dist/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorHandler(error, req, res, next) {
-    const { statusCode, message } = error;
-    res.status(statusCode || 500).json({ message: message });
-}
-exports.default = errorHandler;
